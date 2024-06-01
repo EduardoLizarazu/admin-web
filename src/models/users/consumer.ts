8f1894fdf350ea5bae22f0a7f2bebc7c89b002c7
eduardolizarazu <jelo2021@gmail.com>
2024-06-01 12:50:22 -0400

    craete consumer table

--- src/models/users/consumer.ts
+++ src/models/users/consumer.ts
@@ -11,7 +11,6 @@ export class ConsumerModel implements IConsumer {
     lastName: string;
     email: string;
     registerDate: Date;
-    address: string;
     phone: string;
     birthDate: Date;
     profilePicture: string;
@@ -27,13 +26,12 @@ export class ConsumerModel implements IConsumer {
         this.lastName = consumer.lastName;
         this.email = consumer.email;
         this.registerDate = consumer.registerDate;
-        this.address = consumer.address;
         this.phone = consumer.phone;
         this.birthDate = consumer.birthDate;
         this.profilePicture = consumer.profilePicture;
         this.apartmentNumber = consumer.apartmentNumber;
         this.userCategory = consumer.userCategory;
         this.condominium = consumer.condominium;
-        
+
     }
 }
--- src/models/users/user.ts
+++ src/models/users/user.ts
@@ -4,7 +4,6 @@ interface IUser {
     lastName: string;
     email: string;
     registerDate: Date;
-    address: string;
     phone: string;
     birthDate: Date;
     profilePicture: string;
