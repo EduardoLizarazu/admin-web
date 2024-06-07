cf8f770119114334de0eeb54fd88dadc212e91bc
eduardolizarazu <jelo2021@gmail.com>
2024-06-06 20:05:33 -0400

    login ui created

---
+++ src/app/login/loginForm.tsx
@@ -0,0 +1,67 @@
+"use client";
+import React, { useState } from "react";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    // Aquí podrías agregar la lógica para manejar el inicio de sesión
+    console.log("Login attempt:", email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="space-y-6">
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Email
+        </label>
+        <div className="mt-1">
+          <input
+            id="email"
+            name="email"
+            type="email"
+            autoComplete="email"
+            required
+            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+      </div>
+      <div>
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Password
+        </label>
+        <div className="mt-1">
+          <input
+            id="password"
+            name="password"
+            type="password"
+            autoComplete="current-password"
+            required
+            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            value={password}
+            onChange={(e: any) => setPassword(e.target)}
+          />
+        </div>
+      </div>
+
+      <div>
+        <button
+          type="submit"
+          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Log In
+        </button>
+      </div>
+    </form>
+  );
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,11 @@
+import LoginForm from "./loginForm";
+
 export default function Login() {
-    return (
-        <div>
-        <h1>Login</h1>
-        </div>
-    );
+  return (
+    <div className="flex min-h-screen bg-gray-100 justify-center items-center">
+      <div className="p-8 bg-white shadow-sm rounded-lg">
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
