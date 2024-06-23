ead6ee53e8c660f13872d0dfec74448db682718c
eduardolizarazu <jelo2021@gmail.com>
2024-06-23 15:26:14 -0400

    transformacion a table el form

--- src/app/membership/create/page.tsx
+++ src/app/membership/create/page.tsx
@@ -1,14 +1,23 @@
 "use client";
 import React from "react";
 import { Input, Textarea, Button, Card } from "@nextui-org/react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@nextui-org/react";
+
 import { MembershipTypeEntity } from "app/entities/payment/membershipType.entity";
 import { createMembershipTypeAction } from "./actions/create.action";
 
 export default function MembershipCreatePage() {
   const [type, setType] = React.useState<string>("");
   const [description, setDescription] = React.useState<string>("");
-  const [price, setPrice] = React.useState<number>(0);
-  const [duration, setDuration] = React.useState<number>(0);
+  const [price, setPrice] = React.useState<string>("");
+  const [duration, setDuration] = React.useState<string>("");
   const [details, setDetails] = React.useState<string>("");
 
   const dummyMembership = {
@@ -28,8 +37,8 @@ export default function MembershipCreatePage() {
       id: "0",
       type: type,
       description: description,
-      price: price,
-      duration: duration,
+      price: parseFloat(price),
+      duration: parseInt(duration),
       details: details,
     };
     const membershipEntity = MembershipTypeEntity.create(membershipPrimitive);
@@ -40,58 +49,79 @@ export default function MembershipCreatePage() {
   return (
     <div className="w-full h-screen overflow-x-hidden border-t flex flex-col px-4 sm:px-8 md:px-16">
       <h1>Membership Create Page</h1>
-        <Card className="p-4">
-          <form
-            onSubmit={handleCreation}
-            style={{ display: "flex", flexDirection: "column", gap: "20px" }}
-          >
-            <Input
-              isClearable
-              label="Type"
-              fullWidth
-              value={type}
-              name="type"
-              onChange={(e) => setType(e.target.value)}
-            />
-            <Input
-              isClearable
-              label="Description"
-              fullWidth
-              value={description}
-              name="description"
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            <Input
-              isClearable
-              label="Price"
-              fullWidth
-              type="number"
-              value={price.toString()}
-              name="price"
-              onChange={(e) => setPrice(Number(e.target.value))}
-            />
-            <Input
-              isClearable
-              label="Duration (months)"
-              fullWidth
-              type="number"
-              value={duration.toString()}
-              name="duration"
-              onChange={(e) => setDuration(Number(e.target.value))}
-            />
-            <Textarea
-              label="Details"
-              fullWidth
-              value={details}
-              name="details"
-              onChange={(e) => setDetails(e.target.value)}
-            />
-            <Button type="submit" color="primary">
-              Submit
-            </Button>
-          </form>
-        </Card>
-      
+      <Table isStriped>
+        <TableHeader>
+          <TableColumn>FORM</TableColumn>
+          <TableColumn>INPUT</TableColumn>
+        </TableHeader>
+        <TableBody>
+          <TableRow key="1">
+            <TableCell>Tipo:</TableCell>
+            <TableCell>
+              <Input
+                isClearable
+                label="Tipo"
+                fullWidth
+                value={type}
+                name="type"
+                onChange={(e) => setType(e.target.value)}
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow key="2">
+            <TableCell>Descripción:</TableCell>
+            <TableCell>
+              <Input
+                isClearable
+                label="Description"
+                fullWidth
+                value={description}
+                name="description"
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow key="3">
+            <TableCell>Precio:</TableCell>
+            <TableCell>
+              <Input
+                isClearable
+                label="Precio:"
+                fullWidth
+                value={price}
+                name="price"
+                onChange={(e) => setPrice(e.target.value)}
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow key="4">
+            <TableCell>Duración:</TableCell>
+            <TableCell>
+              <Input
+                isClearable
+                label="Duración (Dias):"
+                fullWidth
+                value={duration}
+                name="duration"
+                onChange={(e) => setDuration(e.target.value)}
+              />
+            </TableCell>
+          </TableRow>
+          <TableRow key="5">
+            <TableCell>Detalles:</TableCell>
+            <TableCell>
+              <Input
+                isClearable
+                label="Detalles"
+                fullWidth
+                value={details}
+                name="details"
+                onChange={(e) => setDetails(e.target.value)}
+              />
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
